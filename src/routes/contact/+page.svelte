bb5de44d7b184f91fda367c32400e294c8f194a0
mitja <marko.skace@tutamail.com>
2024-04-04 12:58:43 +0200

    updated Contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,19 +1,28 @@
 <script>
 	import linkedInLogo from '$lib/images/linkedin-icon-3.svg';
+	import upWorkIcon from '$lib/images/upwork-1.svg';
 	import githubIcon from '$lib/images/github-icon-2.svg';
 </script>
 
-<section class="flex flex-col w-screen space-y-12 max-w-5xl">
-	<div class="flex flex-col items-center gap-3">
-		<p>Feel free to contact me here:</p>
-		<a href="https://www.linkedin.com/in/bmitja/" target="_blank"
-			><img src={linkedInLogo} alt="LinkedIn logo" height="32px" width="32px" /></a
-		>
+<section class="flex flex-col justify-center items-center space-y-12 max-w-5xl">
+	<div
+		class="prose prose-h3:text-slate-100 border-b border-violet-300 flex flex-col items-center gap-3"
+	>
+		<h3>Feel free to contact me here:</h3>
 	</div>
-	<div class="flex flex-col items-center gap-3">
-		<p>...and my Github repo:</p>
-		<a href="https://github.com/markoskace1" target="_blank"
-			><img src={githubIcon} alt="GitHub logo" height="32px" width="32px" /></a
+	<div class="flex flex-row items-center gap-5">
+		<a
+			href="https://www.linkedin.com/in/bmitja/"
+			target="_blank"
+			class="hover:-translate-y-2 tranisition duration-300 ease-in-out hover:scale-110"
+			><img src={linkedInLogo} alt="LinkedIn logo" height="40px" width="40px" /></a
+		>
+
+		<a
+			href="https://github.com/markoskace1"
+			target="_blank"
+			class="hover:-translate-y-2 tranisition duration-300 ease-in-out hover:scale-110"
+			><img src={githubIcon} alt="GitHub logo" height="40px" width="40px" /></a
 		>
 	</div>
 </section>
